686e7bf599ddcd039c31dfb1a6eb7497cb485d94
jose <8703199@gmail.com>
2022-06-15 15:09:39 +0800

    test

---
+++ get_key.py
@@ -0,0 +1,52 @@
+import requests
+import json
+
+#generate city list
+def run():
+    j = open('CityCountyData.json','r')
+    data = json.load(j)
+    list = []
+    for x in data:
+        list.append(x['CityEngName'])
+    
+    
+    
+    #get city key
+    	
+    apikey = "pjskPH6AJ2U3RwgBk6cPGF0AMl2tiybh"
+    
+    citykey = []
+    f = open('city.json','w')
+    for x in list:
+        query = {
+            'apikey':apikey,
+            'q': x,
+            'details':'yes'}
+        
+        q = requests.get("http://dataservice.accuweather.com/locations/v1/TW/search", params = query )
+        data = q.json()
+        try:
+            citykey.append({'name':x,'key':data[0]["Key"]})
+        except:
+            citykey.append({'name':x, 'key':""})
+    
+    f.write(json.dumps(citykey))
+    
+    f.close()
+
+
+
+
+
+
+
+
+
+#
+#
+# p = requests.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-7DEBAB12-3501-48EA-8E17-CE913507A987&limit=10&sort=time')
+# print(p.content)
+
+
+# for x in q.content:
+#     print(x['ID'])
---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+# import get_key
+#:
+# get_key.run()
+f = open("city.json","r")
+j = open("forcast.json","w")
+
+cities = json.load(f)
+apikey = "pjskPH6AJ2U3RwgBk6cPGF0AMl2tiybh"
+
+for x in cities:
+    q = requests.get('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/'+x['key'], params={'apikey':apikey})
+    content = q.json()
+    print(content)
+    x['forecast'] = content 
+
+data = json.dumps(cities)
+
+j.write(data)
+
+j.close()
+f.close()
+
+    
+    
