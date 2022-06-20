3024aa35f1b115338bdf407f5e65e341eb206b8d
jose <8703199@gmail.com>
2022-06-20 10:54:56 +0800

    testhunk

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ f = open("city.json", "r")
 j = open("forcast.json", "w")
 
 cities = json.load(f)
+
 apikey = "pjskPH6AJ2U3RwgBk6cPGF0AMl2tiybh"
 url = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"
 for x in cities:
