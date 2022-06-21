e901f2e8a7264891559363322615fc37093cd821
jose <8703199@gmail.com>
2022-06-21 23:30:38 +0800

    main function final version

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
 import json
 
+
 # import get_key
 #
 # get_key.run()
-f = open("forcast.json", "r")
+f = open("forecast.json", "r")
+j = open("test.json", "w")
 
 cities = json.load(f)
 
-apikey = "pjskPH6AJ2U3RwgBk6cPGF0AMl2tiybh"
+apikey = "W41QyGeqWglFxP74ouh10DOKq4asr8IR"
 url = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"
 for x in cities:
     q = requests.get(
@@ -22,9 +24,8 @@ for x in cities:
     x["forecast"] = array
 
 data = json.dumps(cities)
-
-j = open("forcast.json", "w")
 j.write(data)
+
 
 j.close()
 f.close()
